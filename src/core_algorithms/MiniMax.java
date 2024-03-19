ba23dfdcc4b6c88a0de29f1d0d0ad5d2efba890b
Micah Harker <micah.harker@gmail.com>
2024-03-19 14:38:53 -0400

    implement minvalue and maxvalue functions

--- src/core_algorithms/MiniMax.java
+++ src/core_algorithms/MiniMax.java
@@ -2,6 +2,8 @@ package core_algorithms;
 
 public class MiniMax <S, A> {
     private Game<S,A> game;
+
+    public record Best<A>(int value, A action){};
     private final boolean pruning;
 
     public MiniMax(Game<S,A>, game, boolean pruning){
@@ -12,5 +14,43 @@ public class MiniMax <S, A> {
     public A minimaxSearch(S state){
         Best<A> max = maxValue(state);
         return max.action();
+    }
+
+    public Best<A> maxValue(S state){
+        int maxValue = Integer.MIN_VALUE;
+        A maxAction = null;
+        if(game.isTerminal(state)) {
+            maxValue = game.utility(state);
+        }else{
+            for(A action : game.actions(state)){
+                S newState = game.execute(action, state);
+                Best<A> min = minValue(newState);
+                if (min.value() > maxValue){
+                    maxValue = min.value();
+                    maxAction = action;
+                }
+                game.undo(action, newState);
+            }
+        }
+        return new Best<>(maxValue,maxAction);
+    }
+
+    public Best<A> minValue(S state){
+        int minValue = Integer.MAX_VALUE;
+        A minAction = null;
+        if(game.isTerminal(state)) {
+            minValue = game.utility(state);
+        }else{
+            for(A action : game.actions(state)){
+                S newState = game.execute(action, state);
+                Best<A> max = maxValue(newState);
+                if(max.value() < minValue){
+                    minValue = max.value();
+                    minAction = action;
+                }
+                game.undo(action, newState);
+            }
+        }
+        return new Best<>(minValue, minAction);
     }
 }
